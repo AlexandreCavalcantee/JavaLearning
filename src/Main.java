86f9c8abe5c3be9fc6a469ceb7c2da4da8584475
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-03-21 22:57:48 -0300

    alala

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, caralha do programa!");
+        System.out.println("Olá, caralho do programa!");
     }
 }
