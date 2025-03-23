cd4b7f1b8559d654e579eaf855202b140d0bfadd
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-03-22 21:10:19 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
+import java.util.Scanner;
+
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Olá, caralho do programa!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Digite a operação (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        scanner.close();
+        double result;
+
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                return;
+        }
+
+        System.out.println(num1+" "+operator+" "+num2+": "+result);
     }
 }
