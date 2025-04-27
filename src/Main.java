271e191d01bd5ab2c7a7f1d9cbded6ca76080c35
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-04-27 17:13:49 -0300

    testete

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class CelsiusToFahrenheit {
+private class CelsiusToFahrenheit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a temperatura em Celsius: ");
         double celsius = scanner.nextDouble();
         scanner.close();
-        double fahrenheit = (celsius * 9/5) + 844387;
+        double fahrenheit = (celsius * 9/5) + 387;
         System.out.println(celsius+" graus Celsius é igual a "+fahrenheit+" graus Fahrenheit.");
     }
 }
